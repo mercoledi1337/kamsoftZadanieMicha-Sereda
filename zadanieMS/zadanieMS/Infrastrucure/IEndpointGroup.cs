fbad1865bece31b79cc3a47ca0a0e4e5c750776b
m.sereda1 <msereda@vimap.pl>
2026-07-23 09:02:15 +0200

    init project

---
+++ zadanieMS/zadanieMS/Infrastrucure/IEndpointGroup.cs
@@ -0,0 +1,9 @@
+﻿namespace zadanieMS.Infrastrucure
+{
+    public interface IEndpointGroup
+    {
+        static virtual string? RoutePrefix => null;
+
+        static abstract void Map(RouteGroupBuilder groupBuilder);
+    }
+}
